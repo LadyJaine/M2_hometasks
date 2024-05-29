2aad2377d9f2a6cbfdd0d32d29486ea7c2399236
LadyJaine <fullsucsess@gmail.com>
2024-05-29 08:38:37 +0500

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
-first=15
-second=15
-third=15
-if first==second and second==third:
-    print(3)
-elif first==second or second==third or first==third:
-    print(2)
-elif not first==second or second==third or first==third:
-    print(0)
+first = input('Введите первое число ', )
+second = input('Введите второе число ', )
+third = input('Введите третье число ', )
+if first == second and second==third:
+    print('Все числа равны ',3)
+elif first == second or second == third or first == third:
+    print('Два числа равны ',2)
+elif not first == second or second == third or first == third:
+    print('Все числа неравны ',0)
